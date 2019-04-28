6b4818d120ab6455a10c5f50ceaee150df86a63f
袁凤鸣 <yfmingo@163.com>
2019-04-28 10:25:26 +0800

    Add: navLineV

--- FMBaseKit/FMBaseKitNavigationController.h
+++ FMBaseKit/FMBaseKitNavigationController.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface FMBaseKitNavigationController : UINavigationController
+/// 导航栏底部的细线 颜色
+@property (copy, nonatomic) UIColor *navBottomLineColor;
+/// 导航栏的 背景图 或者 背景颜色
+@property (nonatomic, strong) id backgroundImageOrColor;
+
+- (void)hideNavBottomLine;
+- (void)showNavBottomLine;
 
 @end
--- FMBaseKit/FMBaseKitNavigationController.m
+++ FMBaseKit/FMBaseKitNavigationController.m
@@ -7,25 +7,19 @@
 //
 
 #import "FMBaseKitNavigationController.h"
+#define kColorCCC [UIColor colorWithRed:0.80 green:0.80 blue:0.80 alpha:1.00]
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
 
 @interface FMBaseKitNavigationController ()<UIGestureRecognizerDelegate>
+@property (strong, nonatomic) UIView *navLineV;
 
 @end
 
 @implementation FMBaseKitNavigationController
 
--(void)viewWillAppear:(BOOL)animated{
+- (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    __weak __typeof(self)weakSelf = self;
-    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.interactivePopGestureRecognizer.delegate = weakSelf;
-    }
+    [self fm_exchangeNavBottomLine];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,9 +32,85 @@
     
 }
 
+#pragma mark - 导航栏的 背景图 或者 背景颜色
+-(void)setBackgroundImageOrColor:(id)backgroundImageOrColor {
+    if (backgroundImageOrColor == nil) {
+        backgroundImageOrColor = [self imageWithColor:UIColor.clearColor];
+    }else if ([backgroundImageOrColor isKindOfClass:[UIColor class]]) {
+        backgroundImageOrColor = [self imageWithColor:backgroundImageOrColor];
+    }
+    if ([backgroundImageOrColor isKindOfClass:[UIImage class]]) {
+        [self.navigationBar setBackgroundImage:backgroundImageOrColor forBarMetrics:UIBarMetricsDefault];
+    }
+}
+
+#pragma mark - 导航栏底部的细线
+-(void)setNavBottomLineColor:(UIColor *)navBottomLineColor {
+    _navBottomLineColor = navBottomLineColor;
+    if (navBottomLineColor) {
+        [self fm_exchangeNavBottomLine];
+    }
+}
+
+- (void)fm_exchangeNavBottomLine {
+    //藏旧
+    [self hideBorderInView:self.navigationBar];
+    //添新
+//    if (!_navLineV) {
+//
+//    }
+    _navLineV = [[UIView alloc]initWithFrame:CGRectMake(0, 44, kScreenWidth, 1.0 / [UIScreen mainScreen].scale)];
+    UIColor *color = kColorCCC;
+    if (self.navBottomLineColor) {
+        color = self.navBottomLineColor;
+    }
+    _navLineV.backgroundColor = color;
+    [self.navigationBar addSubview:_navLineV];
+}
+
+- (void)hideNavBottomLine{
+    [self hideBorderInView:self.navigationBar];
+    if (_navLineV) {
+        _navLineV.hidden = YES;
+    }
+}
+
+- (void)showNavBottomLine{
+    _navLineV.hidden = NO;
+}
+
+- (void)hideBorderInView:(UIView *)view{
+    if ([view isKindOfClass:[UIImageView class]]
+        && view.frame.size.height <= 1) {
+        view.hidden = YES;
+    }
+    for (UIView *subView in view.subviews) {
+        [self hideBorderInView:subView];
+    }
+}
+
+#pragma mark - 横竖屏
+
+- (BOOL)shouldAutorotate{
+    return [self.visibleViewController shouldAutorotate];
+    
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation{
+    return [self.visibleViewController preferredInterfaceOrientationForPresentation];
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    if (![self.visibleViewController isKindOfClass:[UIAlertController class]]) {//iOS9 UIWebRotatingAlertController
+        return [self.visibleViewController supportedInterfaceOrientations];
+    }else{
+        return UIInterfaceOrientationMaskPortrait;
+    }
+}
+
 #pragma mark - Private Method
 
-+ (UIImage *)imageWithColor:(UIColor *)color {
+- (UIImage *)imageWithColor:(UIColor *)color {
     CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
     UIGraphicsBeginImageContext(rect.size);
     CGContextRef context = UIGraphicsGetCurrentContext();
@@ -55,3 +125,4 @@
 }
 
 @end
+
